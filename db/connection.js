3b5e3f262397d14c88d63bd693f280b816325bcb
Sona <sonali23.1293@gmail.com>
2021-08-05 23:18:46 -0400

    created votes table

--- db/connection.js
+++ db/connection.js
@@ -7,7 +7,7 @@ const db = mysql.createConnection(
       // Your MySQL username,
       user: 'root',
       // Your MySQL password
-      password: '',
+      password: 'Sonali23#',
       database: 'election'
     },
     console.log('Connected to the election database.')
